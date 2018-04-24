640f0e2234d2b514cc3d1fc603c3812325692fdb
Andres Urrea <andres.urreao@gmail.com>
2018-04-23 21:48:06 -0500

    Posts list filtered

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
 import posts from './posts'
 
-
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
-        </div>
-        <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
-        </ul>
-      </div>
-    )
+  constructor(props){
+    super(props)
+    this.state = {
+      search: ''
+    };
+    this.handleSearch = this.handleSearch.bind(this);
   }
-}
+  handleSearch(e){
+    this.setState({
+      search: e.target.value.substr(0,20)
+    });
+  }
+  render() {
+    let filterPosts = posts.filter(
+      (post) => {
+        return post.title.toLowerCase().indexOf(
+          this.state.search.toLowerCase()) !== -1
+      }
+    );
+      return(
+        <div>
+          <div className="filter">
+            <input type="text" placeholder="Ingresa el término de búsqueda" onChange={this.handleSearch.bind(this)}/>
+          </div>
+          <ul>
+           {
+            filterPosts.map((post,id) =>
+            <li key={id}>
+              <a href={post.url}>
+                <img src={post.image} alt={post.title}/>
+              </a>
+              <p>{post.title}</p>
+            </li>) 
+           }
+          </ul>
+        </div>
+      )
+    }
+  }
 
 
 export default App
